a3de7850f371ef9eeb91ba995b6d22aa4f985999
Aaryan <108219888+Yoriichi-sama@users.noreply.github.com>
2025-09-04 17:26:40 +0530

    Update package.swift

--- package.swift
+++ package.swift
@@ -1,1 +1,28 @@
 
+import PackageDescription
+
+let package = Package(
+    name: "Neura",
+    platforms: [
+        .ios(.v13)
+    ],
+    products: [
+        .library(
+            name: "Neura",
+            targets: ["Neura"]
+        ),
+    ],
+    dependencies: [
+
+    ],
+    targets: [
+        .target(
+            name: "Neura",
+            dependencies: []
+        ),
+        .testTarget(
+            name: "NeuraTests",
+            dependencies: ["Neura"]
+        ),
+    ]
+)
